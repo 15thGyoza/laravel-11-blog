6c4e2cbb377fafe39a03ea1dc2ccc54ea9103b8a
Li Rundian <lirundian2010@gmail.com>
2025-05-14 18:33:35 +0800

    用户注册表单

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,13 +4,15 @@
     <div class="bg-light p-3 p-sm-5 rounded">
         <h1>Hello 👋</h1>
         <p class="lead">
-            你现在所看到的是 <a href="https://lustormstout.com">Lirundian's blog</a> 的示例项目主页。
+            你现在所看到的是 <a href="https://lirundian.com">LiRundian's blog</a> 的示例项目主页。
         </p>
         <p>
             一切，将从这里开始。
         </p>
-        <p>
-            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
-        </p>
+        @if(!Auth::check())
+            <p>
+                <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+            </p>
+        @endif
     </div>
 @stop
